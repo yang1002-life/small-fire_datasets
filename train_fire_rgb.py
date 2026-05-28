a8ece7560347badbe8f6fc0c0fd9a893e45611cd
yang1002 <94842684+yang1002-life@users.noreply.github.com>
2026-05-28 21:46:21 +0800

    Update train_fire_rgb.py

--- train_fire_rgb.py
+++ train_fire_rgb.py
@@ -7,12 +7,8 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # model.load('yolo11n.pt') # 加载预训练权重,改进或者做对比实验时候不建议打开，因为用预训练模型整体精度没有很明显的提升
     # model = YOLO(model=r'./ultralytics/cfg_yolo11/YOLO11-Backbone/ConvNeXtv2/yolo11s-CSCConvNeXt.yaml')
-    # model = YOLO(model=r'runs/train/fire_rgb/exp_yolo13/weights/last.pt')
-    model = YOLO(model=r'./ultralytics/cfg/models/djy/yolo11n_50.yaml')
-    # model = YOLO(model=r'./ultralytics/cfg/models/11/yolo11s.yaml')
     model.train(
                 data=r'./ultralytics/cfg/datasets/djy/fire_rgb.yaml',
-                # data=r'./ultralytics/cfg/datasets/djy/RGBT-3M.yaml',
                 imgsz=640,
                 epochs=2,
                 batch=8,
